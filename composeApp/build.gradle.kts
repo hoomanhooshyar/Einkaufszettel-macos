3c32c09d572af28356af998940387d1600a6d8ab
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2026-09-20 15:38:00 +0200

    Fix Google play version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -182,14 +182,14 @@ kotlin {
 
 android {
     namespace = "com.hooman.einkaufszettel"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.hooman.einkaufszettel"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.17"
+        versionCode = 18
+        versionName = "1.18"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     packaging {
