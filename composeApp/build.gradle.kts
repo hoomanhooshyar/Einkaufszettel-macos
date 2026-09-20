6bfdde1677a496b5f64cfdf6af167da9d2228033
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2026-09-20 14:38:58 +0200

    Change Android App Version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -188,8 +188,8 @@ android {
         applicationId = "com.hooman.einkaufszettel"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     packaging {
