536db7eea95433eed5d729a374e435d1adf9e141
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2026-09-20 16:02:54 +0200

    Change Version Android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -182,14 +182,14 @@ kotlin {
 
 android {
     namespace = "com.hooman.einkaufszettel"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.hooman.einkaufszettel"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 19
-        versionName = "1.19"
+        targetSdk = 37
+        versionCode = 21
+        versionName = "1.21"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     packaging {
